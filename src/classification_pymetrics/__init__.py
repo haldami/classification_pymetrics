7f82d332c9ad9162cbc077811ad753043f3ba537
milos-barabas <milos.halda@gmail.com>
2025-12-07 19:02:51 +0100

    Export výjimek z balíčku.

--- src/classification_pymetrics/__init__.py
+++ src/classification_pymetrics/__init__.py
@@ -1,12 +1,13 @@
 from .metrics import compute_metrics, ClassificationResults
 from .io import load_csv
-from .exceptions import ClassificationPymetricsException
+from .exceptions import ClassificationPymetricsException, CPIOException
 
 __all__ = [
     "compute_metrics",
     "ClassificationResults",
     "load_csv",
     "ClassificationPymetricsException",
+    "CPIOException",
 ]
 
 __version__ = "0.1.0"
